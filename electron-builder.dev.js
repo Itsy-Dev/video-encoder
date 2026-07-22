564e484b981f73c0c7a7776f16c1f4deeecd98e7
Itsy-Dev <itsygamestudio@gmail.com>
2026-07-21 21:40:11 -0700

    consolidate package-test into the dev lane

--- electron-builder.dev.js
+++ electron-builder.dev.js
@@ -2,19 +2,19 @@ const basePackage = require("./package.json");
 
 module.exports = {
     ...basePackage.build,
-    appId: "dev.itsy.videoencoder.package-test",
-    productName: "Video Encoder Package Test",
+    appId: "dev.itsy.videoencoder.dev",
+    productName: "Video Encoder Dev",
     artifactName: "${productName}-${version}-${arch}.${ext}",
     directories: {
         ...basePackage.build.directories,
-        output: "dist-package-test"
+        output: "dist-dev"
     },
     extraMetadata: {
-        name: "video-encoder-package-test",
-        productName: "Video Encoder Package Test"
+        name: "video-encoder-dev",
+        productName: "Video Encoder Dev"
     },
     dmg: {
         ...basePackage.build.dmg,
-        title: "Video Encoder Package Test ${version}"
+        title: "Video Encoder Dev ${version}"
     }
 };
